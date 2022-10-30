8f44b121d944c6bff29d586f007ddc71222bb705
adriana.lawrence <adriana.lawrence@elliptic.co>
2022-10-30 11:20:55 +0000

    Task 6 extra 3

---
+++ src/exercise/06.extra-3.js
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [username, setUsername] = React.useState("")
+
+  function handleSubmit(event){
+    event.preventDefault()
+    onSubmitUsername(username)
+  }
+
+  function handleChange(event){
+    const { value } = event.target;
+    setUsername(value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor='usernameInput'>Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} value={username}/>
+      </div>
+      <button type="submit" >Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
