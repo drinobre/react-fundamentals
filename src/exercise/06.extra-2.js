d16976f79a86012376f8d9b49e6076220220bb70
adriana.lawrence <adriana.lawrence@elliptic.co>
2022-10-30 11:06:33 +0000

    Task 6 extra 2

---
+++ src/exercise/06.extra-2.js
@@ -0,0 +1,34 @@
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+const [error, setError] = React.useState(null)
+
+  function handleSubmit(event){
+    const { value } = event.target.elements.usernameInput
+    onSubmitUsername(value)
+  }
+
+  function handleChange(event){
+    const { value } = event.target;
+    const isLowerCase = value === value.toLowerCase();
+    setError(isLowerCase ? null : "Username must be lower case")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor='usernameInput'>Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange}/>
+      </div>
+      <div style={{color: "red"}}>{error}</div>
+      <button disabled={Boolean(error)} type="submit" >Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
